108ae937e960dc6aada1569d5227a80c1ee9a7cf
PurinPongpanich <105426025+HydroXCarbon@users.noreply.github.com>
2024-04-20 13:13:43 +0700

    update store data

---
+++ src/main/java/Project2_135/DataSet.java
@@ -0,0 +1,11 @@
+package Project2_135;
+
+import java.util.HashSet;
+
+public class DataSet {
+    private final HashSet<String> set = new HashSet<String>();
+
+    public void addData(String data){
+        set.add(data);
+    }
+}
--- src/main/java/Project2_135/Main.java
+++ src/main/java/Project2_135/Main.java
@@ -1,27 +1,59 @@
 package Project2_135;
 
 import java.io.File;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        DataSet set = new DataSet();
+        String userChoice = "";
+        boolean quit = false;
         String path = "src/main/java/Project2_135/words/";
         boolean fileFound = false;
         while(!fileFound) {
             try {
+                System.out.print("Enter word file = ");
                 String filename = scanner.nextLine();
                 Scanner fileScanner = new Scanner(new File(path + filename));
-                storeData(fileScanner);
+                storeData(fileScanner,set);
                 fileFound = true;
+                fileScanner.close();
             } catch (Exception e) {
-                e.printStackTrace();
-                //fix test
             }
+        }
+
+        while(!quit){
+            userChoice = scanner.nextLine();
+            switch(userChoice.toLowerCase()){
+                case "q":
+                    quit = true;
+                    break;
+                case "s":
+                    Search(set);
+                    break;
+                case "l":
+                    Ladder(set);
+                    break;
+                default:
+            }
+        }
+        System.out.println();
+    }
+
+    public static void storeData(Scanner fileScanner, DataSet set){
+        while(fileScanner.hasNextLine()){
+            String col = fileScanner.nextLine().trim();
+            set.addData(col);
         }
     }
 
-    public static void storeData(Scanner file){
+    public static void Search(DataSet set){
+
+    }
+
+    public static void Ladder(DataSet set){
 
     }
 }
