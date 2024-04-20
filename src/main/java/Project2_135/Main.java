0674105e9bbfb9ae0ecd2876487142de664918ee
PurinPongpanich <105426025+HydroXCarbon@users.noreply.github.com>
2024-04-20 12:41:51 +0700

    update find file

--- src/main/java/Project2_135/Main.java
+++ src/main/java/Project2_135/Main.java
@@ -1,7 +1,26 @@
 package Project2_135;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello purin!");
+        Scanner scanner = new Scanner(System.in);
+        String path = "src/main/java/Project2_135/words/";
+        boolean fileFound = false;
+        while(!fileFound) {
+            try {
+                String filename = scanner.nextLine();
+                Scanner fileScanner = new Scanner(new File(path + filename));
+                storeData(fileScanner);
+                fileFound = true;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void storeData(Scanner file){
+        
     }
 }
